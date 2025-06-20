2994c6d6b5c8737075e987799ce569bd802fd54b
Fredrik Ullman <42198444+Fredrik93@users.noreply.github.com>
2025-06-20 19:41:33 +0200

    Update Knapsack_v2.java
    wip

--- src/main/java/ete388/Knapsack_v2.java
+++ src/main/java/ete388/Knapsack_v2.java
@@ -25,59 +25,72 @@ import java.util.List;
  *
  */
 public class Knapsack {
+    private int itemsFound =0;
+    private int [] indices = {0,0};
     public static void main(String[] args) {
 
 
-
-
     }
-    int calculateValue(List<int []> listOfItems) {
+    int calculateValue(List<int[]> listOfItems) {
         //we need an arraylist otherwise the List items are immutable
         List<int[]> list = new ArrayList<>(listOfItems);
         int knapsackCapacity = list.getFirst()[0];
         int amountOfRowsToIterateOver = list.getFirst()[1];
-        int [] indexWhereHighestValueWasFound = new int [amountOfRowsToIterateOver];
-        int highestValue =0;
-        int amountOfItemsSummed=0;
-        int currentValue =0;
-        int currentWeight=0;
-        int tempValue= 0;
-        int tempWeight=0;
+        int[] indexWhereHighestValueWasFound = new int[amountOfRowsToIterateOver];
+        int highestValue = 0;
+        int amountOfItemsSummed = 0;
+        int currentValue = 0;
+        int currentWeight = 0;
+        int tempValue = 0;
+        int tempWeight = 0;
         list.removeFirst();
-        for(int i = 0; i<list.size(); i++)
-        {
+        for (int i = 0; i < list.size(); i++) {
             int value = list.get(i)[0];
             int weight = list.get(i)[1];
 
-            if(currentWeight+weight <= knapsackCapacity){
+            if (currentWeight + weight <= knapsackCapacity) {
                 currentValue += value;
                 currentWeight += weight;
-                amountOfItemsSummed ++;
+                amountOfItemsSummed++;
                 tempValue += currentValue;
 
-            } else if(weight <= knapsackCapacity){
-                currentValue =0;
-                currentWeight =0;
+            } else if (weight <= knapsackCapacity) {
+                currentValue = 0;
+                currentWeight = 0;
                 tempValue += value;
                 tempWeight += weight;
                 amountOfItemsSummed = 0;
                 currentValue += tempValue;
                 currentWeight += tempWeight;
-                amountOfItemsSummed ++;
+                amountOfItemsSummed++;
             }
-            if(tempValue > highestValue){
-            highestValue = tempValue;
-            // stopped here. saving correct index (2) for testOk() is not working.
-            indexWhereHighestValueWasFound[i] = i;}
-            tempValue =0;
-            tempWeight =0;
+            if (tempValue > highestValue) {
+                highestValue = tempValue;
+                // stopped here. saving correct index (2) for testOk() is not working.
+                indexWhereHighestValueWasFound = new int[amountOfItemsSummed];
+                if(indexWhereHighestValueWasFound.length == 1){
+                    indexWhereHighestValueWasFound[0] = i;
+                } else{
+                indexWhereHighestValueWasFound[i-1] = i;}
+            }
+            tempValue = 0;
+            tempWeight = 0;
 
         }
         //just for testing
         System.out.println("weight: " + currentWeight + " value: " + currentValue);
         //expected result
-        System.out.println(amountOfItemsSummed + " (found one item)");
+        itemsFound = amountOfItemsSummed;
+        indices = indexWhereHighestValueWasFound;
+        System.out.format("found %s item(s) \n", amountOfItemsSummed);
         System.out.println(Arrays.toString(indexWhereHighestValueWasFound) + " (index where value was found)");
         return currentValue;
+    }
+
+    public int getItemsFound() {
+        return itemsFound;
+    }
+    public int[] getIndices() {
+        return indices;
     }
 }
