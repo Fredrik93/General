3b5d41be0ee6f43557c7cce4df2fe80c2dabd497
Fredrik Ullman <42198444+Fredrik93@users.noreply.github.com>
2025-06-20 19:41:53 +0200

    Create KnapsackTest.java

---
+++ src/main/java/ete388/KnapsackTest.java
@@ -0,0 +1,66 @@
+package se.general.projects;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+public class KnapsackTest {
+
+    @Test
+    public void testOk(){
+
+        int [] i1 =  {5,3};
+        int [] i2 =  {1,5};
+        int [] i3 =  {10,5};
+        int [] i4 =  {100,5};
+
+        Knapsack k = new Knapsack();
+       int actualValue = k.calculateValue(List.of(i1, i2, i3, i4));
+       int itemsFound = k.getItemsFound();
+       int [] indices = k.getIndices();
+       assertThat(actualValue, is(100));
+       assertThat(itemsFound, is(1));
+       assertThat(indices[0], is(2));
+    }
+    @Test
+    public void testOk1(){
+        //this lamost works. the first two indices are incorrect. now shows [0,0,3] should be [1,2,3]
+        int [] i1 =  {6,4};
+        int [] i2 =  {5,4};
+        int [] i3 =  {4,3};
+        int [] i4 =  {3,2};
+        int [] i5 =  {2,1};
+
+        Knapsack k = new Knapsack();
+        int actualValue =  k.calculateValue(List.of(i1,i2,i3,i4,i5));
+        assertThat(actualValue, is(9));
+        int itemsFound = k.getItemsFound();
+        int [] indices = k.getIndices();
+        assertThat(itemsFound, is(3));
+        assertThat(indices[0], is(1));
+        assertThat(indices[1], is(2));
+        assertThat(indices[2], is(3));
+
+    }
+    @Test
+    public void testOk2(){
+//this almost works as well but it needs to find indices. now shows [0,1,3]
+        int [] i1 =  {5,3};
+        int [] i2 =  {1,1};
+        int [] i3 =  {10,1};
+        int [] i4 =  {100,10};
+        int [] i5 =  {100,1};
+
+        Knapsack k = new Knapsack();
+        int actualValue = k.calculateValue(List.of(i1,i2,i3,i4,i5));
+        assertThat(actualValue, is(111));
+        int itemsFound = k.getItemsFound();
+        int [] indices = k.getIndices();
+        assertThat(itemsFound, is(3));
+        assertThat(indices[1], is(1));
+        assertThat(indices[2], is(10));
+        assertThat(indices[4], is(100));
+
+    }
+}
